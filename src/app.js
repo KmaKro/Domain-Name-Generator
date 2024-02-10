411bc2b7faebdbd5206e1f1cb14c8fa0e0cc21a1
Raul Alvarez <155668921+KmaKro@users.noreply.github.com>
2024-02-10 16:26:31 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -8,4 +8,19 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com"];
+
+  for (let e = 0; e < domain.length; e++) {
+    for (let n = 0; n < noun.length; n++) {
+      for (let a = 0; a < adj.length; a++) {
+        for (let p = 0; p < pronoun.length; p++) {
+          console.log(pronoun[p] + adj[a] + noun[n] + domain[e]);
+        }
+      }
+    }
+  }
 };
